9b195391b107cbd0c6fc0aef20b19f97677d2e5a
Serpe05 <8293352@itisrossi.vi.it>
2024-03-05 22:27:27 +0100

    SISTEMATO
    Se hai dei problemi vai nella cronologia e copiati ed incolalti i pezzi
    
    Co-Authored-By: Al3k6245 <117723650+Al3k6245@users.noreply.github.com>

--- SitoGym/index.php
+++ SitoGym/index.php
@@ -3,8 +3,14 @@ session_start();
 
 require('php/gestioneSegreteria.php');
 
+if(!isset($_SESSION['Utenti']))   //conterrà i codici fiscali degli utenti
+    $_SESSION['Utenti'];    
+if (!isset($_SESSION['Utenti'])) {
+    //conterrà i codici fiscali degli utenti
+    $_SESSION['Utenti'] = "";
+    unset($_SESSION['Utenti']);
+}
 
-    $_SESSION['Utenti'];
 
 ?>
 
@@ -17,9 +23,16 @@ require('php/gestioneSegreteria.php');
     <title>GymWebSite</title>
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <meta content="Webflow" name="generator" />
+    <link href="css/segreteriaStyle.css" rel="stylesheet" type="text/css" />
+    <script src="javascript/gestioneAjaxSegreteria.js"></script>
     <script>
-    <link href="https://assets-global.website-files.com/img/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-    <link href="https://assets-global.website-files.com/img/webclip.png" rel="apple-touch-icon" />
+        type = "text/javascript" > ! function(o, c) {
+            var n = c.documentElement,
+                t = " w-mod-";
+            n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+        }(window, document);
+    </script>
+
 </head>
 
 <body class="body">
@@ -28,9 +41,16 @@ require('php/gestioneSegreteria.php');
             <div class="container-titolo">
                 <h1 class="h1">Dashboard</h1>
             </div>
+            <div class="navigazione"><a href="#" class="container-sezioni w-button">Overview</a><a href="#Clienti"
+                    class="container-sezioni clienti w-button">Clienti</a><a href="#"
+                    class="container-sezioni allenatori w-button">Allenatori</a><a href="#"
+                    class="container-sezioni personale w-button">Personale</a>
+                <form action="/search" class="search allineato w-form"><input class="search-input w-input"
+                        maxlength="256" name="query" placeholder="Search…" type="search" id="Ricerca"
+                        required="" /><input type="submit" class="search-button w-button" value="Search" /></form>
             </div>
 
-            <section id="Clienti" class="sezione"> //PULSANTE CLIENTI
+            <section id="Clienti" class="sezione"> <! //PULSANTE CLIENTI ->
                 <div class="tabella-preset intestazione">
                     <div id="w-node-_7a4c6629-342e-5245-d2aa-78ab28c0b1a4-8abcad94" class="ordinamento"><a id="w-node-b93641eb-4e20-97e4-b67a-f8a3cfa75ef0-8abcad94" href="#" class="icon w-button">Button T\ext</a><a id="w-node-_8ff9f816-7a02-1223-d680-7b0a78005161-8abcad94" href="#" class="ordinamentoupdown w-button">Nome</a></div>
                     <div id="w-node-f9085910-da57-6f33-8ef5-d7a036b922fd-8abcad94" class="tabella-intestazioni">Data di
@@ -55,7 +75,7 @@ require('php/gestioneSegreteria.php');
 
 
 
-            <section class="sezione" id="Allenatori"> //PULSANTE ALLENATORI
+            <section class="sezione" id="Allenatori"> <!//PULSANTE ALLENATORI->
                 <div class="tabella-preset intestazione">
                     <div id="w-node-c6f7797d-88a6-66c5-3210-b528f2cf39f1-8abcad94" class="ordinamento"><a id="w-node-c6f7797d-88a6-66c5-3210-b528f2cf39f2-8abcad94" href="#" class="icon w-button">Button T\ext</a><a id="w-node-c6f7797d-88a6-66c5-3210-b528f2cf39f4-8abcad94" href="#" class="ordinamentoupdown w-button">Nome</a></div>
                     <div id="w-node-c6f7797d-88a6-66c5-3210-b528f2cf39f6-8abcad94" class="tabella-intestazioni">Valutazione</div>
@@ -72,17 +92,22 @@ require('php/gestioneSegreteria.php');
                             <div class="tabella-intestazioni abbonamento">Trainer</div>
                         </div>
                     </div>
+                    <! //QUI SOTTO CI SONO LE VALUTAZIONI IN STELLE TUTTE E 5 SULLA STESSA RIGA->
                     <div id="w-node-_0e779d34-7823-49d5-40c4-5ffff40550ee-8abcad94" class="action stars"><img src="https://assets-global.website-files.com/65db228c551539358abcad8e/65e6d02cb1d41240f646949d_star.png" loading="lazy" alt="" class="star"><img src="https://assets-global.website-files.com/65db228c551539358abcad8e/65e6d02cb1d41240f646949d_star.png" loading="lazy" alt="" class="star"><img src="https://assets-global.website-files.com/65db228c551539358abcad8e/65e6d02cb1d41240f646949d_star.png" loading="lazy" alt="" class="star"><img src="https://assets-global.website-files.com/65db228c551539358abcad8e/65e6d02cb1d41240f646949d_star.png" loading="lazy" alt="" class="star"><img src="https://assets-global.website-files.com/65db228c551539358abcad8e/65e6d02cb1d41240f646949d_star.png" loading="lazy" alt="" class="star"></div>
+                    <! //CERTIFICATO->
                     <div id="w-node-c6f7797d-88a6-66c5-3210-b528f2cf3a0a-8abcad94" class="action"><a id="AddCertificato" href="#" class="icon add w-button">Button Text</a><a href="#" class="icon download w-button">Button Text</a></div>
+                    <! //TURNI->
                     <div id="w-node-e2dab09d-b38f-8e66-1aef-091b33b2299f-8abcad94" class="action"><a id="AddCertificato" href="#" class="icon turni w-button">Button Text</a><a href="#" class="icon addturni w-button">Button Text</a></div>
+                    <! //STATO->
                     <div id="w-node-c6f7797d-88a6-66c5-3210-b528f2cf3a11-8abcad94" class="action"><a href="#" class="icon allerta w-button">Button Text</a><a href="#" class="icon pericolo w-button">Button Text</a></div>
+                    <!  //SOLITI BOTTONI->
                     <div id="w-node-c6f7797d-88a6-66c5-3210-b528f2cf3a16-8abcad94" class="action"><a data-w-id="c6f7797d-88a6-66c5-3210-b528f2cf3a17" href="#" class="icon userdescrizioni w-button">Button Text</a><a href="#" class="icon useremail w-button">Button Text</a><a href="#" class="icon userremove w-button">Button Text</a></div>
                 </div>
             </section>
 
 
 
-            <section class="sezione" id="Tecnici"> //PULSANTE TECNICI
+            <section class="sezione" id="Tecnici"><! //PULSANTE TECNICI->
                 <div class="tabella-preset intestazione">
                     <div id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a186756-8abcad94" class="ordinamento"><a id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a186757-8abcad94" href="#" class="icon w-button">Button T\ext</a><a id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a186759-8abcad94" href="#" class="ordinamentoupdown w-button">Nome</a></div>
                     <div id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a18675b-8abcad94" class="tabella-intestazioni"></div>
@@ -99,13 +124,19 @@ require('php/gestioneSegreteria.php');
                             <div class="tabella-intestazioni abbonamento">Tecnogym</div>
                         </div>
                     </div>
-                    <div id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a18676d-8abcad94" class="action stars"></div>
+                    <! //RIMANGONO PER CREARE GLI SPAZZI NELLA TABELLA->
+                    <div id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a18676d-8abcad94" class="action"></div>
                     <div id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a186773-8abcad94" class="action"></div>
                     <div id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a186778-8abcad94" class="action"></div>
+                    <! //=============->
+                    <!  //VISUALIZZA INTEVENTI VA CONNESSO ALL'HOVER SELECTION INFONDO QUELLO GIUSTO OBV->
                     <div id="w-node-_111048e3-30a4-1baf-272f-ec6dc7bca0ea-8abcad94" class="action"><a id="Interventi" href="#" class="icon interventi w-button">Button Text</a></div>
+                    <!  //SOLITI BOTTONI->
                     <div id="w-node-ed621fd0-2ab0-9e83-5b52-f24c8a186782-8abcad94" class="action"><a data-w-id="ed621fd0-2ab0-9e83-5b52-f24c8a186783" href="#" class="icon userdescrizioni w-button">Button Text</a><a href="#" class="icon useremail w-button">Button Text</a><a href="#" class="icon userremove w-button">Button Text</a></div>
                 </div>
             </section>
+        </div>
+
         </div>
     </section>
     <div style="display:none" class="menu-descrizioni">
@@ -113,11 +144,19 @@ require('php/gestioneSegreteria.php');
         <div class="descrizione">AddCliente</div>
     </div>
     <nav data-w-id="93085ae0-f7f2-eb6d-a80c-8ff237c72caf" class="menu-icone">
-            <div class="badge-4">
-                <div class="text-21">9</div>
-            </div>
+    <div class="notifications-3"><img
+                src="https://assets-global.website-files.com/65db228c551539358abcad8e/65db2680c536bfa0459743bc_Vectors-Wrapper.svg"
+                loading="lazy" width="24" height="24" alt="" class="vectors-wrapper-2" />
+        <div class="badge-4">
+            <div class="text-21">9</div>
+        </div>
         </div><a href="#" class="menu-sezioni _0 w-button"></a><a href="#" class="menu-sezioni _1 w-button"></a>
     </nav>
+    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=65db228c551539358abcad8e"
+        type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
+    <script src="https://assets-global.website-files.com/65db228c551539358abcad8e/js/webflow.399e61ad1.js"
+        type="text/javascript"></script>
 </body>
 
 </html>
